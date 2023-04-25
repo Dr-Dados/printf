fa29ff79fccc89c635ce39441f2f68073dea1e7a
Imadpro69 <imadeddine0.0es@gmail.com>
2023-04-25 06:45:13 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,45 +1,51 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "main.h"
-
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+va_list args;
+int count = 0;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    count += putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    count += printf("%s", va_arg(args, char *));
-                    break;
-                case '%':
-                    count += putchar('%');
-                    break;
-                case 'd':
-                case 'i':
-                    count += printf("%d", va_arg(args, int));
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+while (*format)
+{
+if (*format == '%')
+{
+format++;
 
-    va_end(args);
-    return count;
+switch (*format)
+{
+case 'c':
+count += putchar(va_arg(args, int));
+break;
+case 's':
+count += printf("%s", va_arg(args, char *));
+break;
+case '%':
+count += putchar('%');
+break;
+case 'd':
+case 'i':
+count += printf("%d", va_arg(args, int));
+break;
+default:
+putchar('%');
+putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+
+va_end(args);
+return (count);
 }
 
