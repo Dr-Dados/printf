9db6376dbc1e93c7c708ca2df24a643357ec484e
Dr-Dados <adnane.bachchar@gmail.com>
2023-04-25 08:40:07 -0700

    last changes

--- _printf.c
+++ _printf.c
@@ -9,10 +9,12 @@
  */
 void print_char(va_list args, int *count)
 {
-	int c = va_arg(args, int);
+int c;
 
-	putchar(c);
-	(*count)++;
+c= va_arg(args, int);
+
+putchar(c);
+(*count)++;
 }
 
 /**
@@ -22,13 +24,13 @@ void print_char(va_list args, int *count)
  */
 void print_string(va_list args, int *count)
 {
-	char *s = va_arg(args, char *);
+        char *s = va_arg(args, char *);
 
-	for (; *s; s++)
-	{
-		putchar(*s);
-		(*count)++;
-	}
+        for (; *s; s++)
+        {
+                putchar(*s);
+                (*count)++;
+        }
 }
 
 /**
@@ -37,10 +39,65 @@ void print_string(va_list args, int *count)
  */
 void print_percent(int *count)
 {
-	putchar('%');
-	(*count)++;
+        putchar('%');
+        (*count)++;
+}
+/**
+ * print_decimal - prints a decimal number to stdout
+ * @args: variable arguments list
+ * @count: pointer to character count
+ */
+void print_decimal(va_list args, int *count)
+{
+  int temp, i, digit;
+        int n = va_arg(args, int);
+        int digits = 0;
+
+        if (n == 0)
+        {
+                putchar('0');
+                (*count)++;
+                return;
+        }
+
+        if (n < 0)
+        {
+                putchar('-');
+                (*count)++;
+                n = -n;
+        }
+
+         temp = n;
+        while (temp != 0)
+        {
+                digits++;
+                temp /= 10;
+        }
+
+        while (digits > 0)
+        {
+                int divisor = 1;
+                for ( i = 1; i < digits; i++)
+                {
+                        divisor *= 10;
+                }
+                digit = n / divisor;
+                n %= divisor;
+                digits--;
+                putchar(digit + '0');
+                (*count)++;
+        }
 }
 
+/**
+ * print_integer - prints an integer to stdout
+ * @args: variable arguments list
+ * @count: pointer to character count
+ */
+void print_integer(va_list args, int *count)
+{
+        print_decimal(args, count);
+}
 /**
  * _printf - prints formatted output to stdout
  * @format: format string
@@ -52,39 +109,43 @@ int _printf(const char *format, ...)
 int count = 0;
 va_list args;
 
-	va_start(args, format);
+        va_start(args, format);
 
 
-	for (; *format; format++)
-	{
-		if (*format != '%')
-		{
-			putchar(*format);
-			count++;
-			continue;
-		}
+        for (; *format; format++)
+        {
+                if (*format != '%')
+                {
+                        putchar(*format);
+                        count++;
+                        continue;
+                }
 
-		format++;
+                format++;
 
-		switch (*format)
-		{
-			case 'c':
-				print_char(args, &count);
-				break;
-			case 's':
-				print_string(args, &count);
-				break;
-			case '%':
-				print_percent(&count);
-				break;
-			default:
-				putchar('%');
-				putchar(*format);
-				count += 2;
-				break;
-		}
-	}
+                switch (*format)
+                {
+                        case 'c':
+                                print_char(args, &count);
+                                break;
+                        case 's':
+                                print_string(args, &count);
+                                break;
+                        case '%':
+                                print_percent(&count);
+                                break;
+				case 'd':
+        case 'i':
+                print_integer(args, &count);
+                break;
+                        default:
+                                putchar('%');
+                                putchar(*format);
+                                count += 2;
+                                break;
+                }
+        }
 
-	va_end(args);
-	return (count);
+        va_end(args);
+return (count);
 }
