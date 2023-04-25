0dabb92be5609bb6d65f79cfaadecb700c7d911f
Imadpro69 <imadeddine0.0es@gmail.com>
2023-04-24 18:05:40 -0700

    update

---
+++ _printf.c
@@ -0,0 +1,42 @@
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+
+            switch (*format) {
+                case 'c': {
+                    char c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    fputs(s, stdout);
+                    count += strlen(s);
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
